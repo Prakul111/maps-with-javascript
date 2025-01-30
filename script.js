f0a7a5506c2d1f174363d5392a1a5346e52dbe9e
@Prakul111 <iamprakul111@gmail.com>
2025-01-31 05:01:52 +0530

    finally

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiaWFtcHJha3VsIiwiYSI6ImNtNmpkeXJxbTBpaXcyanFzOG8yYm5jamIifQ.X1wpUHjEZ3Mqcu6VcD7bjw";
 
-const map = new mapboxgl.Map({
-    accessToken: MAPBOX_ACCESS_TOKEN,
-    container: "map", // container ID
-    style: "mapbox://styles/mapbox/streets-v12", // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
-});
-
-
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, { enableHighAccuracy: true })
 
-const styles = StyleSheet.create({ RTCCertificatehrt })
+function setupMap(centerPosition) {
+    const map = new mapboxgl.Map({
+        accessToken: MAPBOX_ACCESS_TOKEN,
+        container: "map", // container ID
+        style: "mapbox://styles/mapbox/streets-v12", // style URL
+        zoom: 12, // starting zoom
+        center: centerPosition,
+    })
+
+    const navigationControls = new mapboxgl.NavigationControl();
+    map.addControl(navigationControls)
+
+    const directionControls = new MapboxDirections({
+        accessToken: MAPBOX_ACCESS_TOKEN
+    })
+    map.addControl(directionControls, "top-left")
+}
+
+function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+    setupMap([-2.24, 53.48])
+
+}
+
+
